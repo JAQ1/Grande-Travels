acde8caed4b905d8f0e5f684ccca67084753fa6f
JAQ1 <jackasaniGit1@gmail.com>
2017-08-10 07:07:10 +1000

    upload project
    first upload

---
+++ GrandeTravels/src/GrandeTravels/Services/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace GrandeTravels.Services
+{
+    interface IRepository<T>
+    {
+        void Create(T entity);
+
+        IEnumerable<T> GetAll();
+
+        T GetSingle(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> Query(Expression<Func<T, bool>> predicate);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ GrandeTravels/src/GrandeTravels/Services/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrandeTravels.Services
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Package> TblPackage { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TravelDB;Trusted_Connection=True;");
+        }
+    }
+}
