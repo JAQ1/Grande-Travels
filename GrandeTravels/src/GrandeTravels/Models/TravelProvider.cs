2ce55fb0e96940ac5549039b8793116a40ecd400
Jack Asani <jackasanigit1@gmail.com>
2017-08-17 15:04:23 +1000

    pre branch commit

---
+++ GrandeTravels/src/GrandeTravels/Models/TravelProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandeTravels.Models
+{
+    public class TravelProvider
+    {
+        public int TravelProviderID { get; set; }
+
+    }
+}
