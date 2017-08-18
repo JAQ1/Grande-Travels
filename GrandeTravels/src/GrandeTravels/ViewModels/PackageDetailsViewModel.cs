033ad5ecc6bbfc5843f88dd6cadbbaf8f3783910
Jack Asani <jackasanigit1@gmail.com>
2017-08-18 15:31:44 +1000

    package details func working(very basic)

---
+++ GrandeTravels/src/GrandeTravels/ViewModels/PackageDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrandeTravels.Models;
+
+namespace GrandeTravels.ViewModels
+{
+    public class PackageDetailsViewModel
+    {
+        public Package Package { get; set; }
+    }
+}
