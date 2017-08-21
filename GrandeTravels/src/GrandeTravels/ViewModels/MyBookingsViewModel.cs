d022c351c3df1834e2efa7bffb549457ce8c2537
Jack Asani <jackasanigit1@gmail.com>
2017-08-21 14:56:32 +1000

    view flow done

---
+++ GrandeTravels/src/GrandeTravels/ViewModels/MyBookingsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrandeTravels.Models;
+
+namespace GrandeTravels.ViewModels
+{
+    public class MyBookingsViewModel
+    {
+        public IEnumerable<Booking> MyBookings { get; set; }
+    }
+}
